061a50f64cbcb9a399c522a257aea728bbffdeb9
art0007i <art0007i@gmail.com>
2023-11-03 03:20:41 +0100

    bump version

--- TypePicker/TypePicker.cs
+++ TypePicker/TypePicker.cs
@@ -14,7 +14,7 @@ namespace TypePicker
     {
         public override string Name => "TypePicker";
         public override string Author => "TheJebForge"; // Ported by art0007i :)
-        public override string Version => "1.0.0";
+        public override string Version => "2.0.0";
 
         public override void OnEngineInit()
         {
