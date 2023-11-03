47642253bbe40e472395c0fba17cb81b9421b207
art0007i <art0007i@gmail.com>
2023-11-03 08:12:37 +0100

    port to resonite :D (#2)
    * Port to resonite
    
    * bump version
    
    * actually handle multi generics

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Type Picker
 
-A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/) that adds type picker into ComponentAttacher 
+A [ResoniteModLoader](https://github.com/resonite-modding-group/ResoniteModLoader) mod for [Resonite](https://resonite.com/) that adds type picker into ComponentAttacher 
 
 Just drop any reference into the picker and choose the type
 ![image](screenshot/panel.png)
 
 ## Installation
-1. Install [NeosModLoader](https://github.com/zkxs/NeosModLoader).
-2. Place [TypePicker.dll](https://github.com/TheJebForge/TypePicker/releases/latest/download/TypePicker.dll) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
-3. Start the game. If you want to verify that the mod is working you can check your Neos logs.
+1. Install [ResoniteModLoader](https://github.com/resonite-modding-group/ResoniteModLoader).
+2. Place [TypePicker.dll](https://github.com/TheJebForge/TypePicker/releases/latest/download/TypePicker.dll) into your `rml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\Resonite\rml_mods` for a default install. You can create it if it's missing, or if you launch the game once with ResoniteModLoader installed it will create the folder for you.
+3. Start the game. If you want to verify that the mod is working you can check your Resonite logs.
--- TypePicker/TypePicker.cs
+++ TypePicker/TypePicker.cs
@@ -1,5 +1,5 @@
 ﻿using HarmonyLib;
-using NeosModLoader;
+using ResoniteModLoader;
 using FrooxEngine;
 using FrooxEngine.UIX;
 using System;
@@ -10,38 +10,69 @@ using System.Reflection.Emit;
 
 namespace TypePicker
 {
-    public class TypePicker : NeosMod
+    public class TypePicker : ResoniteMod
     {
         public override string Name => "TypePicker";
-        public override string Author => "TheJebForge";
-        public override string Version => "1.0.0";
+        public override string Author => "TheJebForge"; // Ported by art0007i :)
+        public override string Version => "2.0.0";
 
-        public override void OnEngineInit() {
+        public override void OnEngineInit()
+        {
             Harmony harmony = new Harmony($"net.{Author}.{Name}");
             harmony.PatchAll();
         }
 
-        public static void BuildComponentAttacherUI(ComponentAttacher componentAttacher, UIBuilder ui) {
-            SyncRef<TextField> field = (SyncRef<TextField>)componentAttacher.GetSyncMember("_customGenericType");
+        public static void BuildComponentSelectorUI(ComponentSelector selector, object uiDisplayClass)
+        {
 
-            ReferenceField<IWorldElement> refField = componentAttacher.FindNearestParent<Slot>().AttachComponent<ReferenceField<IWorldElement>>();
-            
+            var ui = Traverse.Create(uiDisplayClass).Field<UIBuilder>("ui").Value;
+            var fields = (SyncRefList<TextField>)selector.GetSyncMember("_customGenericArguments");
+
+            ReferenceField<IWorldElement> refField = selector.FindNearestParent<Slot>().GetComponentOrAttach<ReferenceField<IWorldElement>>();
+
+            // max amount of generic params is ushort.MaxValue (I checked this myself)
+            var paramIndex = selector.FindNearestParent<Slot>().GetComponentOrAttach<ValueField<ushort>>().Value;
+            paramIndex.Value = 0;
+
+            // Make the Generic Param Name clickable to select which one you put into.
+            selector.RunInUpdates(1, () => {
+                // if there's only a single there's no point in generating this
+                if (fields.Count <= 1) return;
+                for (int i = 0; i < fields.Count; i++)
+                {
+                    var f = fields[i];
+                    var textSlot = f.Slot.Parent.Parent[0][0];
+                    var btn = textSlot.AttachComponent<Button>();
+                    var text = textSlot.GetComponent<Text>();
+                    btn.SetupBackgroundColor(text.Color);
+                    btn.ColorDrivers.FirstOrDefault().DisabledColor.Value = RadiantUI_Constants.Hero.GREEN;
+                    var radio = textSlot.AttachComponent<ValueRadio<ushort>>();
+                    radio.OptionValue.Value = (ushort)i;
+                    radio.TargetValue.Target = paramIndex;
+                    var bvd = textSlot.AttachComponent<BooleanValueDriver<bool>>();
+                    bvd.FalseValue.Value = true;
+                    bvd.TrueValue.Value = false;
+                    bvd.TargetField.Target = btn.EnabledField;
+                    radio.CheckVisual.Target = bvd.State;
+                }
+            });
+
             SyncMemberEditorBuilder.Build(refField.Reference, "Type picker", null, ui);
             ui.HorizontalLayout(8f);
             {
-                ui.Button("Base type").LocalPressed += (button, data) => SetType(field, FindBaseType(refField));
-                ui.Button("Inner type").LocalPressed += (button, data) => SetType(field, FindInnerType(refField));
+                ui.Button("Base type").LocalPressed += (button, data) => SetType(fields[paramIndex.Value], FindBaseType(refField));
+                ui.Button("Inner type").LocalPressed += (button, data) => SetType(fields[paramIndex.Value], FindInnerType(refField));
             }
             ui.NestOut();
-            
+
             ui.Text("Cast to:");
-            
+
             ui.HorizontalLayout(8f);
             {
-                ui.Button("SyncRef").LocalPressed += (button, data) => SetType(field, CastToSyncRef(refField));
-                ui.Button("SyncRef Inner").LocalPressed += (button, data) => SetType(field, CastToSyncRefInner(refField));
-                ui.Button("IField").LocalPressed += (button, data) => SetType(field, CastToIField(refField));
-                ui.Button("IField Inner").LocalPressed += (button, data) => SetType(field, CastToIFieldInner(refField));
+                ui.Button("SyncRef").LocalPressed += (button, data) => SetType(fields[paramIndex.Value], CastToSyncRef(refField));
+                ui.Button("SyncRef Inner").LocalPressed += (button, data) => SetType(fields[paramIndex.Value], CastToSyncRefInner(refField));
+                ui.Button("IField").LocalPressed += (button, data) => SetType(fields[paramIndex.Value], CastToIField(refField));
+                ui.Button("IField Inner").LocalPressed += (button, data) => SetType(fields[paramIndex.Value], CastToIFieldInner(refField));
             }
             ui.NestOut();
         }
@@ -49,12 +80,12 @@ namespace TypePicker
         static Type FindBaseType(ReferenceField<IWorldElement> refField) {
             try {
                 return refField.Reference.Target.GetType();
-            }
+            } 
             catch (Exception) {
                 return null;
             }
         }
-        
+
         static Type FindInnerType(ReferenceField<IWorldElement> refField) {
             try {
                 return FindBaseType(refField).GenericTypeArguments[0];
@@ -72,7 +103,7 @@ namespace TypePicker
                 return null;
             }
         }
-        
+
         static Type CastToSyncRefInner(ReferenceField<IWorldElement> refField) {
             try {
                 return CastToSyncRef(refField).GenericTypeArguments[0];
@@ -81,7 +112,7 @@ namespace TypePicker
                 return null;
             }
         }
-        
+
         static Type CastToIField(ReferenceField<IWorldElement> refField) {
             try {
                 return typeof(IField<>).MakeGenericType(((IField)refField.Reference.Target).ValueType);
@@ -90,7 +121,7 @@ namespace TypePicker
                 return null;
             }
         }
-        
+
         static Type CastToIFieldInner(ReferenceField<IWorldElement> refField) {
             try {
                 return CastToIField(refField).GenericTypeArguments[0];
@@ -100,41 +131,49 @@ namespace TypePicker
             }
         }
 
-        static void SetType(SyncRef<TextField> field, Type type) {
+        static void SetType(TextField field, Type type) {
             try {
-                field.Target.Editor.Target.Text.Target.Text = type.FullName;
+                field.Editor.Target.Text.Target.Text = type.FullName;
             }
             catch (Exception) {
                 // ignored
             }
         }
 
-        [HarmonyPatch(typeof(ComponentAttacher), "BuildUI")]
-        class ComponentAttacher_BuildUI_Patch
+        [HarmonyPatch(typeof(ComponentSelector), "BuildUI")]
+        class ComponentSelector_BuildUI_Patch
         {
-            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) {
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
                 int startIndex = -1;
 
                 List<CodeInstruction> codes = new List<CodeInstruction>(instructions);
 
-                for (int i = 0; i < codes.Count; i++) {
+                for (int i = 0; i < codes.Count; i++)
+                {
                     CodeInstruction instr = codes[i];
-                    if (instr.opcode != OpCodes.Ldstr || !((string)instr.operand).Contains("Custom Generic Type Name")) continue;
-                    Msg("Found!");
+                    //this string appears twice, we need the first occurance in this case
+                    if (instr.opcode != OpCodes.Ldstr || !((string)instr.operand).Contains("ComponentSelector.CustomGenericArguments")) continue;
+                    Debug("Found code at index " + i);
                     startIndex = i - 1;
                     break;
                 }
 
-                if (startIndex > -1) {
-                    MethodInfo method = typeof(TypePicker).GetMethod("BuildComponentAttacherUI", BindingFlags.Public | BindingFlags.Static);
-                    
-                    codes.InsertRange(startIndex, new []
+                if (startIndex > -1)
+                {
+                    MethodInfo method = typeof(TypePicker).GetMethod("BuildComponentSelectorUI", BindingFlags.Public | BindingFlags.Static);
+
+                    codes.InsertRange(startIndex, new[]
                     {
                        new CodeInstruction(OpCodes.Ldarg_0),
                        new CodeInstruction(OpCodes.Ldloc_0),
                        new CodeInstruction(OpCodes.Call, method)
                     });
-                    Msg("Patched");
+                    Debug("Patched");
+                }
+                else
+                {
+                    Warn("Could not find patch target! This means the mod won't do anything.");
                 }
 
                 return codes.AsEnumerable();
